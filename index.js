38b3c84d32bebb158c5100d1e5855f8534bac7ae
0xmorningstar <orszukpqt4398@hotmail.com>
2025-03-02 18:24:52 +0800

    add date filter options

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const { execSync } = require('child_process');
 const { program } = require('commander');
 const { calculateStats, printStats } = require('./stats');
 
-function getGitLog(path = '.') {
+function getGitLog(path = '.', options = {}) {
   try {
-    const output = execSync(`git -C ${path} log --pretty=format:"%H|%an|%ae|%ad|%s" --date=iso`, {
-      encoding: 'utf8'
-    });
+    let cmd = `git -C ${path} log --pretty=format:"%H|%an|%ae|%ad|%s" --date=iso`;
+
+    if (options.since) {
+      cmd += ` --since="${options.since}"`;
+    }
+    if (options.until) {
+      cmd += ` --until="${options.until}"`;
+    }
+
+    const output = execSync(cmd, { encoding: 'utf8' });
     return output.split('\n').filter(line => line.trim());
   } catch (err) {
     console.error('Error reading git log:', err.message);
@@ -28,8 +35,10 @@ program
   .description('Analyze git repository statistics')
   .version('0.1.0')
   .argument('[path]', 'path to git repository', '.')
-  .action((path) => {
-    const logs = getGitLog(path);
+  .option('-s, --since <date>', 'show commits more recent than date')
+  .option('-u, --until <date>', 'show commits older than date')
+  .action((path, options) => {
+    const logs = getGitLog(path, options);
     const commits = parseCommits(logs);
     const stats = calculateStats(commits);
     printStats(stats);
